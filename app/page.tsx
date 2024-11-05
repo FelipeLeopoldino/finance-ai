5d815bd0a3f4859a80b1cb9def8983068bc065f6
FelipeLeopoldino <felipeleopoldino01@icloud.com>
2024-11-05 18:47:06 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Hello, Next.js!</h1>
+    <div className="flex items-center justify-center w-screen">
+      <h1 className="text-red-500 p-5">Hello, Next.js!</h1>
     </div>
   );
 };
